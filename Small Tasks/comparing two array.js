d27c3d6e329e5eb70e5f7b17020f6abfd38f5308
Kumar Atul <atultunnu2@gmail.com>
2020-04-02 14:25:19 +0530

    Please contribute

--- Small Tasks/comparing two array.js
+++
@@ -1,8 +0,0 @@
-arr1=[1,2,'a'];
-arr2=[1,2,'a'];
-console.log(arr1==arr2);  //always false
-console.log(arr1+""==arr2+""); //true when array or any other objects are added to "", they are also converted to string
-console.log(arr1+"", typeof arr1+"");
-
-//other way
-console.log(String(arr1)==String(arr2));
